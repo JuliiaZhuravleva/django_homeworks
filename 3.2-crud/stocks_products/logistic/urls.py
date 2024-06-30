98b2842fc483a1735c71fbe62bdb993bd8bed611
Julia Zhuravleva <zhuravlevajuliia@gmail.com>
2024-06-30 16:59:50 +0400

    Исправлен синтаксис в urls

--- 3.2-crud/stocks_products/logistic/urls.py
+++ 3.2-crud/stocks_products/logistic/urls.py
@@ -1,12 +1,13 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path
 
-from logistic.views import ProductViewSet, StockViewSet
+from logistic.views import ProductViewSet, StockViewSet, test_view
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('stocks', StockViewSet)
 
 urlpatterns = router.urls
-urlpatterns += [ 
+urlpatterns += [
     path('test/', test_view, name='test_view'),
 ]
